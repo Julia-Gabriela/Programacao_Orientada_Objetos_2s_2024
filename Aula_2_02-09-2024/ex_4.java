a2a4093551dfeeb7051b13a1bcce89a0c13e0dad
-Julia-Gabriela <-juliagabrielagomes09@gmail>
2024-09-02 11:43:28 -0300

    Aula java

---
+++ Aula_2_02-09-2024/ex_4.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+
+public class ex_4
+{
+	public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+
+        int segundo;
+
+        System.out.println("escreva os segundos:");
+        segundo = input.nextInt();
+        
+
+        int hora = segundo / 60;
+    }
+}
